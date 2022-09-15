4438da2dcb579809bc51814b6d00018739ce515e
netlag <n3tlag@outlook.com>
2022-09-15 16:28:26 -0400

    Handled destroyed structs on the map view

--- js/pages/components/UIStructMapView.js
+++ js/pages/components/UIStructMapView.js
@@ -34,26 +34,62 @@ export class UIStructMapView {
     return `${wholeHeartsHtml}${halfHeartHtml}${emptyHeartsHtml}`;
   }
 
-  render() {
-    let isDefending = '';
-    let isDefended = '';
+  /**
+   * @return {string}
+   */
+  renderIsDefendingIcon() {
+    return (this.struct.defending)
+      ? `<img src="${IMG.ICONS}icon-rook.png" alt="rook">`
+      : '';
+  }
+
+  /**
+   * @return {string}
+   */
+  renderIsDefendedIcon() {
+    return (this.struct.defenders.length > 0)
+      ? `<img src="${IMG.ICONS}icon-strength.png" alt="strength">`
+      : '';
+  }
+
+  /**
+   * @return {string}
+   */
+  renderIsHiddenIcon() {
     let isHidden = '';
-
-    if (this.struct.defending) {
-      isDefending = `<img src="${IMG.ICONS}icon-rook.png" alt="rook">`;
-    }
-
-    if (this.struct.defenders.length > 0) {
-      isDefended = `<img src="${IMG.ICONS}icon-strength.png" alt="strength">`;
-    }
-
     if (this.struct.defenseComponent.name === DEFENSE_COMPONENTS.STEALTH_MODE) {
       isHidden = this.struct.defenseComponent.isActive
         ? `<img src="${IMG.ICONS}icon-invisible.png" alt="invisible">`
         : `<img src="${IMG.ICONS}icon-visible.png" alt="visible">`;
     }
+    return isHidden;
+  }
 
+  /**
+   * @return {string}
+   */
+  renderIsDestroyedIcon() {
+    return (this.struct.isDestroyed)
+      ? `<img src="${IMG.ICONS}icon-skull.png" alt="skull">`
+      : '';
+  }
+
+  /**
+   * @return {string}
+   */
+  renderStatusIcons() {
+    let statusIcons =  this.renderIsDefendingIcon()
+      + this.renderIsDefendedIcon()
+      + this.renderIsHiddenIcon();
+    if (this.struct.isDestroyed) {
+      statusIcons = this.renderIsDestroyedIcon();
+    }
+    return statusIcons;
+  }
+
+  render() {
     return `
+      ${this.struct.isDestroyed ? '' : `
       <a
         class="struct-map-view-btn"
         data-player-id="${this.player.id}"
@@ -62,12 +98,15 @@ export class UIStructMapView {
         href="javascript: void(0)"
         role="button"
       >
-        <div class="struct">
+      `}
+        <div class="struct ${this.struct.isDestroyed ? 'destroyed' : ''}">
           <div>${this.renderHealthInHearts()}</div>
           <img src="${this.struct.image}" alt="${this.struct.unitType}">
-          <div>${isDefending}${isDefended}${isHidden}</div>
+          <div>${this.renderStatusIcons()}</div>
         </div>
+      ${this.struct.isDestroyed ? '' : `
       </a>
+      `}
     `;
   }
 }
