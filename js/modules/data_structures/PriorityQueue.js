51020229e044f9350afaf67cc7c9ec2f21b7a0d1
netlag <n3tlag@outlook.com>
2022-09-29 16:19:28 -0400

    Priority queue dequeue

--- js/modules/data_structures/PriorityQueue.js
+++ js/modules/data_structures/PriorityQueue.js
@@ -8,12 +8,19 @@ export class PriorityQueue {
    */
   insert(node) {
     for (let i = 0; i < this.queue.length; i++) {
-      if (node.priority < this.queue[i].priority) {
+      if (node.priority > this.queue[i].priority) {
         this.queue.splice(i, 0, node);
         return;
       }
     }
     this.queue.push(node);
+  }
+
+  /**
+   * @return {PriorityQueueNode}
+   */
+  dequeue() {
+    return this.queue.pop();
   }
 
   /**
--- test/js/vendor/PriorityQueueTest.js
+++ test/js/vendor/PriorityQueueTest.js
@@ -18,22 +18,49 @@ const insertTest = new DTest('insertTest', function() {
   queue.insert(node4);
   queue.insert(node2);
 
-  this.assertEquals(queue.getLength(), 3)
-  this.assertEquals(queue.queue[0].value, 'banana');
+  this.assertEquals(queue.getLength(), 3);
+  this.assertEquals(queue.queue[0].value, 'dragon fruit');
   this.assertEquals(queue.queue[1].value, 'clementine');
-  this.assertEquals(queue.queue[2].value, 'dragon fruit');
+  this.assertEquals(queue.queue[2].value, 'banana');
 
   queue.insert(node1);
   queue.insert(node5);
 
-  this.assertEquals(queue.getLength(), 5)
-  this.assertEquals(queue.queue[0].value, 'apple');
-  this.assertEquals(queue.queue[1].value, 'banana');
+  this.assertEquals(queue.getLength(), 5);
+  this.assertEquals(queue.queue[0].value, 'elderberry');
+  this.assertEquals(queue.queue[1].value, 'dragon fruit');
   this.assertEquals(queue.queue[2].value, 'clementine');
-  this.assertEquals(queue.queue[3].value, 'dragon fruit');
-  this.assertEquals(queue.queue[4].value, 'elderberry');
+  this.assertEquals(queue.queue[3].value, 'banana');
+  this.assertEquals(queue.queue[4].value, 'apple');
+});
+
+const dequeueTest = new DTest('dequeueTest', function() {
+  const queue = new PriorityQueue();
+  const node1 = new PriorityQueueNode(1, 'apple');
+  const node2 = new PriorityQueueNode(2, 'banana');
+  const node3 = new PriorityQueueNode(3, 'clementine');
+  const node4 = new PriorityQueueNode(4, 'dragon fruit');
+  const node5 = new PriorityQueueNode(5, 'elderberry');
+
+
+  queue.insert(node3);
+  queue.insert(node4);
+  queue.insert(node5);
+
+  this.assertEquals(queue.dequeue().value, 'clementine');
+
+  queue.insert(node1);
+
+  this.assertEquals(queue.dequeue().value, 'apple');
+  this.assertEquals(queue.dequeue().value, 'dragon fruit');
+
+  queue.insert(node2);
+
+  this.assertEquals(queue.dequeue().value, 'banana');
+  this.assertEquals(queue.dequeue().value, 'elderberry');
 });
 
 // Test execution
 console.log('PriorityQueueTest');
 insertTest.run();
+dequeueTest.run();
