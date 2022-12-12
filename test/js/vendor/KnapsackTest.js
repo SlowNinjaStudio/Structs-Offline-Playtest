f555952d53cc991d0c99e93a865bae92d4901675
netlag <n3tlag@outlook.com>
2022-12-12 11:46:08 -0500

    Knapsack brute force test

--- test/js/vendor/KnapsackTest.js
+++ test/js/vendor/KnapsackTest.js
@@ -91,7 +91,91 @@ const unboundedTest = new DTest('unboundedTest', function(params) {
   ];
 });
 
+const bruteForceTest = new DTest('bruteForceTest', function(params) {
+  const solution = (new Knapsack()).bruteForce(params.values, params.weights, params.target, params.maxItems);
+
+  this.assertEquals(solution.sum, params.expectedSum);
+  this.assertSetEquality(solution.values, params.expectedValues);
+  this.assertSetEquality(solution.weights, params.expectedWeights);
+}, function() {
+  return [
+    {
+      values: [],
+      weights: [],
+      target: 0,
+      maxItems: 0,
+      expectedValues: [],
+      expectedWeights: [],
+      expectedSum: 0
+    },
+    {
+      values: [1],
+      weights: [1],
+      target: 1,
+      maxItems: 1,
+      expectedValues: [1],
+      expectedWeights: [1],
+      expectedSum: 1
+    },
+    {
+      values: [1],
+      weights: [1],
+      target: 2,
+      maxItems: 2,
+      expectedValues: [1, 1],
+      expectedWeights: [1, 1],
+      expectedSum: 2
+    },
+    {
+      values: [1, 2],
+      weights: [1, 2],
+      target: 2,
+      maxItems: 1,
+      expectedValues: [2],
+      expectedWeights: [2],
+      expectedSum: 2
+    },
+    {
+      values: [1.5, 2, 3.5],
+      weights: [1, 2, 3],
+      target: 5,
+      maxItems: 3,
+      expectedValues: [3.5, 1.5, 1.5],
+      expectedWeights: [3, 1, 1],
+      expectedSum: 6.5
+    },
+    {
+      values: [1.5, 2.5, 3.5],
+      weights: [2, 2, 4],
+      target: 16,
+      maxItems: 4,
+      expectedValues: [3.5, 3.5, 3.5, 3.5],
+      expectedWeights: [4, 4, 4, 4],
+      expectedSum: 14
+    },
+    {
+      values: [1.5, 2.5, 3.5],
+      weights: [2, 2, 4],
+      target: 50,
+      maxItems: 4,
+      expectedValues: [3.5, 3.5, 3.5, 3.5],
+      expectedWeights: [4, 4, 4, 4],
+      expectedSum: 14
+    },
+    {
+      values: [1.5, 2.5, 3.5],
+      weights: [2, 2, 4],
+      target: 14,
+      maxItems: 4,
+      expectedValues: [3.5, 3.5, 3.5, 2.5],
+      expectedWeights: [4, 4, 4, 2],
+      expectedSum: 13
+    },
+  ];
+});
+
 // Test execution
 console.log('KnapsackTest');
 createLookupTableTest.run();
 unboundedTest.run();
+bruteForceTest.run();
